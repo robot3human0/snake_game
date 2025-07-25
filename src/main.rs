fe322a0945c51acbb1da5d0a9dead2d158f02486
Dilmurod <d.ismailov@oson.com>
2025-07-25 14:17:59 +0500

    feat:  - Added tests,  - added constant FRAME_PER_SECOND fix:  - value of constant INITIAL_SNAKE_BODY

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,9 @@ use std::iter::FromIterator;
 const CELL_SIZE: i32 = 20;                                      // размер ячейки 20 пикселей
 const GRID_SIZE: i32 = 20;                                      // Размер гряды/столбца 20 ячеек (400 пикселей)
 const BOARD_SIZE: u32 = (GRID_SIZE * GRID_SIZE) as u32;         // Размер игрового поля 400 (х 400) пикселей
-const INITIAL_SNAKE_BODY: &[(i32, i32)] = &[(0, 0), (0, 1)];    // Место на доске где появляется новая змейка (две ячейки по горизонтали в левом верхнем углу)
-
-#[derive(Clone, PartialEq)]
+const INITIAL_SNAKE_BODY: &[(i32, i32)] = &[(1, 0), (0, 0)];    // Место на доске где появляется новая змейка (две ячейки по горизонтали в левом верхнем углу)
+const FRAMES_PER_SECOND: u64 = 6;                               // Частота перерисовки кадров в секунду (скорость змейки)
+#[derive(Clone, PartialEq, Debug)]
 enum Direction {
     Right, Left, Up, Down
 }
@@ -206,7 +206,7 @@ fn main() {
     let food = Food::new_random(&snake.body);
     let mut game = Game {gl, snake, food };
 
-    let mut events = Events::new(EventSettings::new()).ups(6);
+    let mut events = Events::new(EventSettings::new()).ups(FRAMES_PER_SECOND);
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
             game.render(&r);
@@ -221,5 +221,124 @@ fn main() {
                 game.pressed(&k.button);
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn snake_move_right() {
+        let mut snake = Snake::new();
+
+        let prev_head = snake.body.front().unwrap().clone();
+
+        snake.direction = Direction::Right;
+        snake.update();
+
+        let current_head = snake.body.front().unwrap().clone();
+
+        assert_ne!(prev_head, current_head);
+        assert_eq!(snake.direction, Direction::Right);
+    }
+
+    #[test]
+    fn snake_move_down() {
+        let mut snake = Snake::new();
+
+        let prev_head = snake.body.front().unwrap().clone();
+
+        snake.direction = Direction::Down;
+        snake.update();
+
+        let current_head = snake.body.front().unwrap().clone();
+
+        assert_ne!(prev_head, current_head);
+        assert_eq!(snake.direction, Direction::Down);
+    }
+
+    #[test]
+    fn snake_move_left() {
+        // Для этого теста нужно создать кастомную змейку, потому что оригинальная движется в право
+        // при рождении, соответственно мы не можем повернуть сразу влево.
+        // Поэтому мы в этом тесте создаем змейку, которая движется вниз изначально!
+        let mut snake = Snake {
+            body: LinkedList::from_iter([(0, 1), (0, 0)]),
+            direction: Direction::Down,
+            growing: false,
+        };
+
+        let prev_head = snake.body.front().unwrap().clone();
+
+        snake.direction = Direction::Left;
+        snake.update();
+
+        let current_head = snake.body.front().unwrap().clone();
+
+        assert_ne!(prev_head, current_head);
+        assert_eq!(snake.direction, Direction::Left);
+    }
+
+    #[test]
+    fn snake_move_up() {
+        let mut snake = Snake::new();
+
+        let prev_head = snake.body.front().unwrap().clone();
+
+        snake.direction = Direction::Up;
+        snake.update();
+
+        let current_head = snake.body.front().unwrap().clone();
+
+        assert_ne!(prev_head, current_head);
+        assert_eq!(snake.direction, Direction::Up);
+    }
+
+    #[test]
+    fn snake_growth() {
+        let mut snake = Snake::new();
+
+        let initial_length = snake.body.len();
+
+        snake.grow();
+        snake.update();
+
+        assert_ne!(initial_length, snake.body.len());
+    }
+
+    #[test]
+    fn self_collision() {
+        let mut snake = Snake {
+            growing: false,
+            direction: Direction::Up,
+            body: LinkedList::from_iter([(0, 1), (1, 1), (1, 0), (0, 0)])
+        };
+
+        let result = snake.update();
+
+        assert!(!result, "Snake should collide with itself");
+    }
+
+    #[test]
+    fn snake_wraps_around() {
+        let mut snake = Snake::new();
+
+        snake.direction = Direction::Up;
+        snake.update();
+
+        let head_should_be = (1, GRID_SIZE -1);
+        // start head is (1, 0)
+        let head = snake.body.front().unwrap().clone();
+
+        assert_eq!(head, head_should_be);
+    }
+
+    #[test]
+    fn food_coordinates_in_bounds() {
+        let food = Food::new_random(&LinkedList::from_iter(vec![]));
+
+        assert!(food.x >= 0 && food.x < GRID_SIZE);
+        assert!(food.y >= 0 && food.y < GRID_SIZE);
     }
 }
